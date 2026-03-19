dba3e132aef637c87372e8748bc3d6eedf95fdb0
Sunil Kumar Singh <158507694+sunil8654@users.noreply.github.com>
2026-03-19 22:58:23 +0530

    Update page.tsx

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import ArticlePage from './ArticleClient';
 
 export const dynamicParams = true;
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export async function generateStaticParams() {
   try {
